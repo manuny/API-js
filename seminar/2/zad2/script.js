db5927b345f7ca15e705c2248800a1d3c410a1d3
Vasileva Masha <Vasilevamasha1982@gmail.com>
2024-04-24 20:37:09 +0300

    zad2

---
+++ seminar/2/zad2/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const btnEl = document.querySelector(".btn");
+const modalEl = document.querySelector(".modal");
+
+btnEl.addEventListener("click", () => {
+  modalEl.classList.add("active");
+});
+
+const closeEl = document.querySelector(".close");
+closeEl.addEventListener("click", () => {
+  modalEl.classList.remove("active");
+});
